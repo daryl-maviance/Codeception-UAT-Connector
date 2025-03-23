393ec9a9d09285ffcc5d878e114740cf5552a19d
Nfoye Daryl <nfoyedewilde@gmail.com>
2025-03-23 16:44:44 +0100

    Refactor API test suite to use ApiTester and enhance health and ping tests

--- tests/Api/CashinCest.php
+++ tests/Api/CashinCest.php
@@ -5,22 +5,166 @@ declare(strict_types=1);
 
 namespace Tests\Api;
 
-use Tests\Support\AcceptanceTester;
+use Tests\Support\ApiTester;
 
 final class CashinCest
 {
-    public function _before(AcceptanceTester $I): void
+    public function _before(ApiTester $I): void
     {
         // Code here will be executed before each test.
         $I->haveHttpHeader('Content-Type', 'application/json');
         $I->haveHttpHeader('Accept', 'application/json');
-        $I->haveHttpHeader('x-api-version','V1');
+        $I->haveHttpHeader('x-api-version','v1');
         $I->haveHttpHeader('x-api-key','TESTKEY');
 
     }
 
-    public function tryToTest(AcceptanceTester $I): void
+    public function TestHealth(ApiTester $I): void
     {
         // Write your tests here. All `public` methods will be executed as tests.
+        $I->wantTo('Make sure  the  service is healthy');
+        $I->sendGet('/health');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType(
+            [
+                'time' => 'string',
+                'workload' => [
+                    'pending' => 'integer',
+                    'success' => 'integer',
+                    'error' => 'integer'
+                ],
+                'remote' =>[
+                    'status' => 'string',
+                    'info' => 'string'
+                ],
+                'components' => [
+                    [
+                        'name' => 'string',
+                        'status' => 'string',
+                        'info' => 'string'
+                    ]
+                ]
+
+            ]
+        );
+        $I->seeResponseContainsJson(
+            [
+                'remote' =>[
+                    'status' => 'OK',
+                    'info' => 'OK'
+                ]
+            ]
+        );
+    }
+
+    public function TestPing(ApiTester $I): void
+    {
+        $I->wantTo('check  the  avaibility of  the api');
+        $I->sendGet('/ping');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType(
+            [
+                'status' => 'string',
+            ]
+        );
+        $I->seeResponseContainsJson(
+            [
+                'status' => 'OK'
+            ]
+        );
+    }
+
+
+    /**
+     * @skip
+     */
+    public function TestCashinPay(ApiTester $I): void
+    {
+        $I->wantTo('Make a cashin payment');
+        $payload =[
+            "cdata"=> "string",
+          
+            "customer"=> [
+              "phone"=> "+23699255753",
+              "email"=> "address@domain.com",
+              "firstname"=> "John",
+              "lastname"=> "Doe",
+              "dob"=> "2001-05-17",
+              "idDocumentNumber"=> "string",
+              "idDocumentType"=> "string",
+              "idDocumentCountryIso"=> "CM",
+              "cdata"=> "string"
+            ],
+            
+            "agent"=> [
+              "agentId"=> "string",
+              "ccId"=> "dae566",
+              "agentName"=> "Daryl",
+              "ccName"=> "Daryl Sarl",
+              "lat"=> 85,
+              "lng"=> 180
+            ],
+            "service" => "MOMO_CASHIN",
+            "callbackUrl"=> "https://www.smxobilpay.example.com",
+            "destination"=> "079255753",
+            "ptn"=> "BG",
+            "amount"=> 1000
+        ];
+        $I->sendPost('/');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType(
+            [
+               
+            ]
+        );
+        $I->seeResponseContainsJson(
+            [
+               
+            ]
+        );
+    }
+
+    public function Testlookup (ApiTester $I): void
+    {
+        $I->wantTo('search  for  customer name');
+        $cident = 'ok';
+        $I->sendGet('/lookup?cident='.$cident);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType(
+            [
+               'found' => 'boolean',
+               'entry' =>[
+                    'cident' => 'string',
+                    'cname'=>'string',
+               ]
+            ]
+        );
+    }
+
+
+
+    /**
+     * @skip
+     */
+    public function Test(ApiTester $I): void
+    {
+        $I->wantTo('');
+        $I->send('/');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType(
+            [
+               
+            ]
+        );
+        $I->seeResponseContainsJson(
+            [
+               
+            ]
+        );
     }
 }
